4fa4bcdbe4f1b538f980e893c6975dcd5f0e2fe4
Lior Ben-David <lior@hotglue.io>
2025-09-25 13:28:43 +0000

    Revert "Merge branch 'feature/hgi-8802' into 'fix/hgi-4344'"

--- tap_magento/client.py
+++ tap_magento/client.py
@@ -336,13 +336,13 @@ class MagentoStream(RESTStream):
     
     def make_store_filter(self, store_id):
         if isinstance(store_id, str):
-            store_id = store_id.replace(" ", "")
+            store_id = store_id.split(",")
         if store_id:
-            return {
-                "searchCriteria[filterGroups][2][filters][0][field]": "store_id",
-                "searchCriteria[filterGroups][2][filters][0][value]": store_id,
-                "searchCriteria[filterGroups][2][filters][0][condition_type]": "in"
-            }
+            params = {}
+            for idx, store in enumerate(store_id):
+                params[f"searchCriteria[filterGroups][2][filters][{idx}][field]"] = "store_id"
+                params[f"searchCriteria[filterGroups][2][filters][{idx}][value]"] = store
+            return params
         return {}
 
     def get_start_date(self):
